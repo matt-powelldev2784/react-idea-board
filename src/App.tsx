2b61684e82f2d7db6b9249470bc7b9a0f2521c1b
Matthew Powell <matt.powell2784@gmail.com>
2024-08-15 20:31:12 +0100

    feat: remove duplicate body tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import styled from 'styled-components'
 import { NavigationBar } from './components/NavigationBar/NavigationBar'
 
 function App() {
   return (
-    <Main>
+    <>
       <NavigationBar />
-    </Main>
+    </>
   )
 }
 
 export default App
-
-const Main = styled.body`
-  position: relative;
-  width: 100%;
-`
