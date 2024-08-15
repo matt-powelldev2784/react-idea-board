e866b34d84bb37b02786fa609013cacc6fee1856
Matthew Powell <matt.powell2784@gmail.com>
2024-08-15 19:18:08 +0100

    style: add logo to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { ReactComponent as Logo } from './assets/images/idb_logo.svg'
+import styled from 'styled-components'
+
 function App() {
-  return <div className="App">test</div>
+  return (
+    <div className="App">
+      <StyledLogo />
+    </div>
+  )
 }
 
 export default App
+
+const StyledLogo = styled(Logo)`
+  width: 210px;
+  height: 65px;
+  padding: 5px;
+`
