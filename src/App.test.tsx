a2f65e0fe964f9d8b2bf8687ae9251d626eb5a9b
Matthew Powell <matt.powell2784@gmail.com>
2024-08-17 07:54:26 +0100

    build: update testing library packages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './theme/theme'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/test/i)
-  expect(linkElement).toBeInTheDocument()
+  render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  )
+  const logoButton = screen.getByLabelText(
+    'IDB Logo - click here to go to homepage'
+  )
+  expect(logoButton).toBeInTheDocument()
 })
