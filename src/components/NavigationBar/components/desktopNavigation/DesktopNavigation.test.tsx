66e1ab661d98f34252915634263533bc83128c46
Matthew Powell <matt.powell2784@gmail.com>
2024-08-17 12:09:37 +0100

    test: fix navigation tests due to hideden items causing issue

--- src/components/NavigationBar/components/desktopNavigation/DesktopNavigation.test.tsx
+++ src/components/NavigationBar/components/desktopNavigation/DesktopNavigation.test.tsx
@@ -3,18 +3,19 @@ import { ThemeProvider } from 'styled-components'
 import { DesktopNavigation } from './DesktopNavigation'
 import { navItemsList } from '../navItemsList/navItemsList'
 import { theme } from '../../../../theme/theme'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('DesktopNavigation', () => {
   test('should render the correct number of NavItem components', () => {
     render(
       <ThemeProvider theme={theme}>
-        <DesktopNavigation />
+        <MemoryRouter>
+          <DesktopNavigation />
+        </MemoryRouter>
       </ThemeProvider>
     )
 
-    const navItems = screen.getAllByRole('listitem')
+    const navItems = screen.getAllByRole('link', { hidden: true })
     expect(navItems.length).toBe(navItemsList.length)
   })
-
-  // Optionally, you can add more tests to check for responsiveness and other behaviors
 })
