d5ee2b0aabd730141f660783635da01d63f7646f
Matthew Powell <matt.powell2784@gmail.com>
2024-08-17 11:14:36 +0100

    test: add nav items list test

---
+++ src/components/NavigationBar/components/navItemsList/navIItemsList.test.tsx
@@ -0,0 +1,9 @@
+import { navItemsList } from './navItemsList'
+
+describe('navItemsList', () => {
+  // This test ensures that the navigation contains less than or equal to 4 items
+  // This is important because the the desktop navigation ui doesn't have enough space to display more than 4 items
+  it('should contain less than or equal to 4 items', () => {
+    expect(navItemsList.length).toBeLessThanOrEqual(4)
+  })
+})
