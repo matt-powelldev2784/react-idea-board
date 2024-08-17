9cdbb7f7b9a51252e3fc52a1976180755439e58e
Matthew Powell <matt.powell2784@gmail.com>
2024-08-17 08:03:08 +0100

    feat: split app test into describe and test block

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,14 +3,17 @@ import App from './App'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './theme/theme'
 
-test('renders learn react link', () => {
-  render(
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  )
-  const logoButton = screen.getByLabelText(
-    'IDB Logo - click here to go to homepage'
-  )
-  expect(logoButton).toBeInTheDocument()
+
+describe('App Component', () => {
+  test('App renders navigation bar logo', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    )
+    const logoButton = screen.getByLabelText(
+      'IDB Logo - click here to go to homepage'
+    )
+    expect(logoButton).toBeInTheDocument()
+  })
 })
