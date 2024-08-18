b50747720eea99a9675f29e748582f5dc7f89b56
Matthew Powell <matt.powell2784@gmail.com>
2024-08-18 13:14:30 +0100

    test: add button tests

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -9,6 +9,7 @@ const AppRouter = () => (
       <Route path="/" element={<Home />} />
       <Route path="/create-idea" element={<CreateIdea />} />
       <Route path="/idea-list" element={<IdeaList />} />
+      <Route path="/not-found" element={<NotFound />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
