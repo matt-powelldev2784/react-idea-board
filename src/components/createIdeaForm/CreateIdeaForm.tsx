d8e240265140df2b3cff73fc6b16139f0cf21b50
Matthew Powell <matt.powell2784@gmail.com>
2024-08-18 21:45:02 +0100

    feat: add input component to form

--- src/components/createIdeaForm/CreateIdeaForm.tsx
+++ src/components/createIdeaForm/CreateIdeaForm.tsx
@@ -2,15 +2,29 @@ import styled from 'styled-components'
 import { useFormik } from 'formik'
 // import useCreateIdeaFormik from './hooks/useCreateIdeaFormik'
 import { LightBulbOutline } from '../../assets/images/svg'
+import { Input } from '../../ui'
+import * as Yup from 'yup'
 
 export const CreateIdeaForm = () => {
   // const formik = useCreateIdeaFormik()
 
+  const validationSchema = Yup.object({
+    title: Yup.string().required('Title is required'),
+    description: Yup.string()
+      .required('Description is required')
+      .max(140, 'Description must be at most 140 characters'),
+    numOfStars: Yup.number()
+      .min(1, 'Number of stars must be at least 1')
+      .required('Number of stars is required'),
+  })
+
   const formik = useFormik({
     initialValues: {
-      email: '',
+      title: '',
+      description: '',
+      numOfStars: 0,
     },
-
+    validationSchema,
     onSubmit: (values) => {
       console.log(values)
     },
@@ -20,14 +34,31 @@ export const CreateIdeaForm = () => {
     <StyledContainer>
       <StyledForm onSubmit={formik.handleSubmit}>
         <StyledImage as={LightBulbOutline} />
-        <label htmlFor="email">Email Address</label>
+        <Input
+          formik={formik}
+          htmlFor={'title'}
+          id={'title'}
+          name={'title'}
+          type={'text'}
+          labelText={'Title'}
+        />
 
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
+        <Input
+          formik={formik}
+          htmlFor={'description'}
+          id={'description'}
+          name={'description'}
+          type={'text'}
+          labelText={'Description'}
+        />
+
+        <Input
+          formik={formik}
+          htmlFor={'numOfStars'}
+          id={'numOfStars'}
+          name={'numOfStars'}
+          type={'number'}
+          labelText={'Confidence Level (1-5 stars)'}
         />
 
         <button type="submit">Submit</button>
@@ -46,23 +77,18 @@ const StyledContainer = styled.section`
   align-items: flex-start;
 `
 
-// const StyledBackground = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   position: absolute;
-// `
-
 const StyledForm = styled.form`
-  width: 400px;
-  padding: 20px;
+  width: 600px;
+  padding-left: 40px;
+  padding-right: 40px;
   margin: 50px auto;
   background-color: #fff;
-  border-radius: 8px;
+  border-radius: 12px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
+  gap: 5px;
 `
 
 const StyledImage = styled.div`
--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,5 +1,6 @@
 import { PrimaryButton } from './button/PrimaryButton'
 import { SecondaryButton } from './button/SecondaryButton'
 import { CustomButton } from './button/CustomButton'
+import { Input } from './formElements/Input'
 
-export { PrimaryButton, SecondaryButton, CustomButton }
+export { PrimaryButton, SecondaryButton, CustomButton, Input }
