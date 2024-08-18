0878df4ad582cd70217b8f684b15096a153c3d69
Matthew Powell <matt.powell2784@gmail.com>
2024-08-18 15:43:23 +0100

    feat: add text to sample card section

--- src/components/home/sampleCards/SampleCards.tsx
+++ src/components/home/sampleCards/SampleCards.tsx
@@ -14,6 +14,19 @@ export const SampleCards = () => {
 
   return (
     <StyledSectoion>
+      <StyledText>
+        ID:B is your go-to application for turning your creative sparks into
+        actionable plans. Our platform is meticulously designed to foster
+        creativity and innovation, making it the perfect tool for brainstorming
+        your next big project or organizing your thoughts. With ID:B, you can
+        effortlessly capture, refine, and execute your ideas through an
+        intuitive and seamless interface. Explore the potential of ID:B by
+        checking out some sample idea cards at the bottom of this section. These
+        examples showcase how you can use our platform to bring your ideas to
+        life and stay organized. Join us on this journey to transform your
+        creative process and achieve your goals with ease.
+      </StyledText>
+
       {sampleCardsToDisplay.map((card) => {
         return (
           <IdeaCard
@@ -42,3 +55,18 @@ const StyledSectoion = styled.section`
   border-radius: 10px;
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
 `
+
+const StyledText = styled.p`
+  width: 95%;
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.primaryDarkBlue};
+  text-align: center;
+  margin-bottom: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.large}) {
+    font-size: 18px;
+  }
+`
+
