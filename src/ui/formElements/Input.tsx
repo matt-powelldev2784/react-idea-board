23c7b324d0875b3b3fe72bf031ea64739f9362cf
Matthew Powell <matt.powell2784@gmail.com>
2024-08-18 21:44:40 +0100

    feat: add input component

---
+++ src/ui/formElements/Input.tsx
@@ -0,0 +1,77 @@
+import { FormikProps } from 'formik'
+import styled from 'styled-components'
+
+interface InputProps {
+  formik: FormikProps<any>
+  htmlFor: string
+  id: string
+  name: string
+  type: string
+  labelText: string
+}
+
+export const Input = ({
+  formik,
+  htmlFor,
+  id,
+  name,
+  type,
+  labelText,
+}: InputProps) => {
+  const isError = formik.touched[name] && formik.errors[name] ? true : false
+
+  return (
+    <StyledContainer>
+      <StyledLabel htmlFor={htmlFor}>{labelText.toUpperCase()}</StyledLabel>
+
+      <StyledInput
+        id={id}
+        name={name}
+        type={type}
+        onChange={formik.handleChange}
+        value={formik.values[name] as any}
+        isError={isError}
+      />
+
+      {isError ? <ErrorText>Error</ErrorText> : null}
+    </StyledContainer>
+  )
+}
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 85px;
+`
+
+const StyledLabel = styled.label`
+  color: ${({ theme }) => theme.colors.textGrey};
+  margin: 0;
+  padding-left: 2px;
+  color: ${({ theme }) => theme.colors.primaryBlue};
+  font-size: 14px;
+  font-family: 'Roboto_600Bold';
+`
+
+const StyledInput = styled.input<{ isError: boolean }>`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid
+    ${({ theme, isError }) => (isError ? 'red' : theme.colors.boxOutlineGrey)};
+  border-radius: 4px;
+  margin-top: 4px;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.black};
+    outline: none; // Optional: Remove the default focus outline
+  }
+`
+
+const ErrorText = styled.p`
+  color: red;
+  margin: 0;
+  padding-left: 2px;
+  font-size: 14px;
+  text-align: center;
+`
