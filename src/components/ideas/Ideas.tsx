7f804a8f50ee8663564aaa3e7ea9e69f61c7d715
Matthew Powell <matt.powell2784@gmail.com>
2024-08-22 19:33:00 +0100

    style: remove height from styled card container

--- src/components/ideas/Ideas.tsx
+++ src/components/ideas/Ideas.tsx
@@ -84,7 +84,6 @@ const StyledCardContainer = styled.div`
   flex-wrap: wrap;
   width: 100%;
   max-width: 1080px;
-  min-height: 100vh;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.large}) {
   }
